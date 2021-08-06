b2ec6a643793917e2dc4ed03e3f635ac32f8676f
Hardikdigiqt <hardik@digiqt.com>
2021-08-06 17:14:45 +0530

    added dark converter using state

---
+++ src/routes/App/components/App.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import "../styles/_app.scss";
+
+function App() {
+  const [toggle, onToggle] = useState(false);
+  const [cssClass, setCssClass] = useState("app");
+
+  const handleDark = (value) => {
+    onToggle(!value);
+
+    if (value === true) {
+      setCssClass("app dark-mode");
+    }else{
+      setCssClass("app");
+    }
+  };
+
+  return (
+    <div className={cssClass}>
+      <div className="level">
+        <div>
+          <h1 className="title">Dark Mode Challenge</h1>
+        </div>
+
+        {/* --The button that should toggle dark mode-- */}
+        <button
+          className="app__dark-mode-btn icon level-right"
+          onClick={(e) => handleDark(toggle)}
+        >
+          {" "}
+          {toggle ? <FontAwesomeIcon icon={faSun} color="#FFA500" /> : null}
+          {!toggle ? (
+            <div>
+              <FontAwesomeIcon icon={faMoon} />
+            </div>
+          ) : null}
+        </button>
+      </div>
+
+      <div className="columns">
+        <div className="column">
+          <p>
+            Lollipop powder powder. Cotton candy caramels chupa chups halvah
+            muffin caramels apple pie topping cake. Topping chocolate bar pastry
+            chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar
+            plum. Chocolate bar lollipop candy canes. Biscuit croissant apple
+            pie pudding caramels wafer tart tootsie roll macaroon. Croissant
+            tiramisu chocolate bar carrot cake lemon drops halvah.
+          </p>
+        </div>
+        <div className="column">
+          <p>
+            Marshmallow tiramisu liquorice bear claw chocolate bar bear claw
+            tart. Muffin chupa chups pie. Brownie apple pie topping lemon drops
+            marzipan toffee. Pudding macaroon icing ice cream bonbon cake tart.
+            Pudding sugar plum chocolate cake cake biscuit pastry pastry
+            chocolate bar tart. Lemon drops dessert gummies icing.
+          </p>
+        </div>
+      </div>
+
+      <div className="field">
+        <div className="control">
+          <input className="input" type="text" placeholder="Name" />
+        </div>
+      </div>
+
+      <div className="field">
+        <div className="control">
+          <input className="input" type="text" placeholder="Email" />
+        </div>
+      </div>
+
+      <section className="section">
+        <div className="buttons level-right">
+          <a className="button is-primary">Save</a>
+          <a className="button is-link">Submit</a>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
